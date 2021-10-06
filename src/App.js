e6eeb88b4b242fb758abb729d9bbe4c8dcb1f91f
quangTD <tsquang398@gmail.com>
2021-10-06 18:09:10 +0700

    [PRACTICE] :sparkles: add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>day la file App.js</h1>
+      <Header />
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-      <h1>cuoc song ma</h1>
-    </div>
+    <AppBar>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.SearchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search..."
+              className={{
+                root: classes.inputRoot,
+                input: classes.inpputInput
+              }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
