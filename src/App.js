ae6a6c6d854fcc73e768da49470e3e2d70e7163b
quangTD <tsquang398@gmail.com>
2021-10-07 18:57:47 +0700

    [PRACTICE-V1] :art: update layout for App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import Header from './Header/Header';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import Map from './components/Map/Map';
+import List from './components/List/List';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <CssBaseline />
       <Header />
-    </div>
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
